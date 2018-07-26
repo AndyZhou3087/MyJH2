f83e33a22cc4c5ae35300ed291219864d5c3a7a1
zhouj <zhou_jian007@126.com>
2018-07-26 17:40:51 +0800

    ios crash

--- Classes/MapBlockScene.cpp
+++ Classes/MapBlockScene.cpp
@@ -50,9 +50,6 @@ MapBlockScene::~MapBlockScene()
 
 	g_MapBlockScene = NULL;
 
-	if (_fogrender)_fogrender->autorelease();//add, inorder to achieve delay release, use autorelease instead of release
-
-	if (_myrender)_myrender->autorelease();//add, inorder to achieve delay release, use autorelease ins
 }
 
 Scene* MapBlockScene::createScene(std::string mapname)
@@ -477,6 +474,7 @@ void MapBlockScene::updateMyRender(float dt)
 	_mylight->setPosition(myposParticle->getPosition());
 	_mylight->visit();
 	_myrender->end();
+	Director::getInstance()->getRenderer()->render();
 }
 
 void MapBlockScene::ajustMyPos()
@@ -519,6 +517,7 @@ void MapBlockScene::createMyRender()
 	_myrender->addChild(_mylight);
 	_mylight->visit();
 	_myrender->end();
+	Director::getInstance()->getRenderer()->render();
 }
 
 void MapBlockScene::createFog()
@@ -553,6 +552,7 @@ void MapBlockScene::createFog()
 		}
 	}
 	_fogrender->end();
+	Director::getInstance()->getRenderer()->render();
 }
 
 void MapBlockScene::updateFog(float dt)
@@ -562,6 +562,7 @@ void MapBlockScene::updateFog(float dt)
 		_fogrender->begin();
 		addFogBlock(mycurRow, mycurCol);
 		_fogrender->end();
+		Director::getInstance()->getRenderer()->render();
 		map_mapBlocks[mycurRow*blockColCount + mycurCol]->setIsCanSee(true);
 	}
 }
