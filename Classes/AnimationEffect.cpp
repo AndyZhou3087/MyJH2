26e34165f0fc668e65e191e88e2577db41fca9b5
zhouj <zhou_jian007@126.com>
2018-08-14 10:05:57 +0800

    界面弹出动画优化

--- Classes/AnimationEffect.cpp
+++ Classes/AnimationEffect.cpp
@@ -21,7 +21,7 @@ void AnimationEffect::closeAniEffect(cocos2d::Layer* layer)
 	LayerColor* color = (LayerColor*)layer->getChildByName("colorLayer");
 	if (color!=NULL)
 	{
-		color->runAction(FadeOut::create(0.21f));
+		color->runAction(FadeOut::create(0.15f));
 	}
-	layer->runAction(Sequence::create(ScaleTo::create(0.15f, 1.05f), ScaleTo::create(0.1f, 0.6f), Hide::create(), RemoveSelf::create(), NULL));
+	layer->runAction(Sequence::create(ScaleTo::create(0.12f, 1.05f), ScaleTo::create(0.07f, 0.6f), Hide::create(), DelayTime::create(0.03f), RemoveSelf::create(), NULL));
 }
