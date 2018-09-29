16318c92a96afc519ab42fee6126e209f1b23095
zhouj <zhou_jian007@126.com>
2018-09-29 20:25:29 +0800

    引导修改角色名称，不做修改时，不继续引导bug

--- Classes/HeroAttrLayer.cpp
+++ Classes/HeroAttrLayer.cpp
@@ -487,14 +487,12 @@ void HeroAttrLayer::editBoxEditingDidEndWithAction(cocos2d::ui::EditBox* editBox
 	{
 		editBox->setText(m_heroData->getName().c_str());
 		MovingLabel::show(ResourceLang::map_lang["nicknameempty"]);
-		return;
 	}
 	else if (m_heroData->getName().compare(editstr) == 0)
 	{
-		return;
-	}
 
-	if (GlobalInstance::getInstance()->checkifSameName(editBox->getText()))
+	}
+	else if (GlobalInstance::getInstance()->checkifSameName(editBox->getText()))
 	{
 		editBox->setText(m_heroData->getName().c_str());
 		MovingLabel::show(ResourceLang::map_lang["samename"]);
