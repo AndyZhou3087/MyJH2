9fc28a6028e175ae5572267aa3f53a67fe0f5634
zhouj <zhou_jian007@126.com>
2018-09-30 11:11:48 +0800

    主城界面切换到后台，再次进入时不刷新BUG

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -101,6 +101,9 @@ void AppDelegate::applicationDidEnterBackground() {
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
+
+	if (g_mainScene != NULL)
+		HttpDataSwap::init(g_mainScene)->getServerTime();
     // if you use SimpleAudioEngine, it must resume here
     // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
