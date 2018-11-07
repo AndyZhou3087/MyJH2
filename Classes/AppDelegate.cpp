0ca7c25f6fbc4ff44c72e3d22c204191dc9f8fda
kxx_zhou <zhou_@Kxx>
2018-11-07 18:12:58 +0800

    修改刷新帧率

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -55,7 +55,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 	director->setDisplayStats(false);
 
 	// set FPS. the default value is 1.0/60 if you don't call this
-	director->setAnimationInterval(1.0f / 60);
+	director->setAnimationInterval(1.0f / 40);
 
 	// Set the design resolution
 	glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::EXACT_FIT);
