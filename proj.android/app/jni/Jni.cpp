de7b4752e45be9a8d150b63c6374b5e240b55253
zhou <zhou@DESKTOP>
2019-03-15 17:15:49 +0800

    迷宫地图修改

--- proj.android/app/jni/Jni.cpp
+++ proj.android/app/jni/Jni.cpp
@@ -3,16 +3,16 @@
 #include "MainScene.h"
 extern "C"
 {
-	JNIEXPORT void JNICALL Java_com_game_myjh_JNI_sendMessage(JNIEnv *env, jobject obj,jint msg);
-	JNIEXPORT void JNICALL Java_com_game_myjh_JNI_saveAllData(JNIEnv *env, jobject obj);
+	JNIEXPORT void JNICALL Java_com_game_myjh_JNI_sendMessage(JNIEnv *env, jclass jobj,jint msg);
+	JNIEXPORT void JNICALL Java_com_game_myjh_JNI_saveAllData(JNIEnv *env, jclass jobj);
 }
 
-JNIEXPORT void JNICALL Java_com_game_myjh_JNI_sendMessage(JNIEnv *env, jobject obj,jint msg)
+JNIEXPORT void JNICALL Java_com_game_myjh_JNI_sendMessage(JNIEnv *env, jclass jobj,jint msg)
 {
 	ShopLayer::setMessage((PYARET)msg);
 }
 
-JNIEXPORT void JNICALL Java_com_game_myjh_JNI_saveAllData(JNIEnv *env, jobject obj)
+JNIEXPORT void JNICALL Java_com_game_myjh_JNI_saveAllData(JNIEnv *env, jclass jobj)
 {
     MainScene::saveAllData();
 }
